091369957e9bcc8cd3ff093187ae0250181800aa
Ary-dev04 <a3524110169@alumno.uttehuacan.edu.mx>
2026-04-11 11:35:10 -0600

    actualizacion de buscador tratamientos 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\Usuario;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -34,6 +35,9 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('asistente-only', function ($user) {
             return $user->rol === 'asistente';
         });
+        if (config('app.env') !== 'local') {
+        URL::forceScheme('https');
+    }
     
     }
 }
